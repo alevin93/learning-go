ccf4d8fa2cc5e12015630ad647d30546d30d305f
Alex <alex.levin93@gmail.com>
2023-03-09 22:29:56 -0800

    wrote most of sql-bookstore

---
+++ sql-bookstore/cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/alevin93/learning-go/sql-bookstore/pkg/routes"
+)
+
+func main(){
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+	fmt.PrintF("Starting server on port 9010")
+}
